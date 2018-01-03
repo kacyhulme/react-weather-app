254d0f4863a87d2946c6ee612bb09af2af8db57a
Hulme <kacy.hulme@gmail.com>
2018-01-03 13:01:15 -0600

    add a bit of copy

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,19 @@ var React = require('react');
 
 var About = () => {
  return (  
-  <h3> About Component </h3>
+  <div>
+  <h1 className="text-center"> About</h1>
+  <p>More information here</p>
+  <ul>
+<li>
+<a href="https://facebook.github.io/react">React</a> - This was the JS framework used.
+</li>
+<li>
+<a href="http://openweathermap.org">Open Weather Map</a> - Used Open Weather Map to search for weather.
+</li>
+<li></li>
+  </ul>
+  </div>
   )
 };
 module.exports = About;
