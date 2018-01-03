96c3d7f14d6641c5b158e455973a052774aca197
Hulme <kacy.hulme@gmail.com>
2018-01-03 11:29:12 -0600

    add a bit of style

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,17 +1,23 @@
 var React = require('react');
-
-// var Examples = React.createClass({
-//   render: function() {
-//     return (  
-//       <h3> Examples Component </h3>
-//       )
-//   }
-// });
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (  
-      <h3> Examples Component </h3>
-      )
+    <div>
+    <h1 className="text-centered"> Examples Component </h1>
+    <p>Here are a few Example Locations to try</p>
+    <ol>
+    <li>
+    <Link to='/?location=Philadelphia'>Philadelphia,PA</Link>
+    </li>
+    <li>
+    <Link to='/?location=Rio'>Rio, Brazil</Link>
+
+    </li>
+    <li></li>
+    </ol>
+    </div>
+    )
 }
 
 
